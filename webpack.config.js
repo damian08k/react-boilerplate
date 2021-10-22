e0dc720d6deab6d16223bc8d7ebc9fa93b79b491
damian08k <damiankierzkowski08@wp.pl>
2021-10-22 18:59:43 +0200

    Add new plugins and loaders

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,19 @@
 const path = require('path');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: 'development',
     entry: path.resolve(__dirname, 'src') + '/index.tsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'js/main.js',
+        filename: 'main.js',
+        clean: true,
     },
     devtool: 'source-map',
+    target: 'web',
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.jsx'],
     },
@@ -39,6 +43,25 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.css$/i,
+                use: [MiniCSSExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCSSExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            sourceMap: true,
+                            importLoader: 2,
+                        },
+                    },
+                    'sass-loader',
+                ],
+            },
         ],
     },
     plugins: [
@@ -46,5 +69,21 @@ module.exports = {
             template: path.join(__dirname, 'src', 'index.html'),
         }),
         new ForkTsCheckerWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.join(__dirname, 'src', 'assets'),
+                    to: 'assests',
+                },
+            ],
+        }),
+        new ESLintPlugin({
+            extensions: ['ts', 'tsx', 'js', 'jsx'],
+            exclude: 'node_modules',
+        }),
+        new MiniCSSExtractPlugin({
+            filename: 'dist/[name].css',
+            chunkFilename: '[name].css',
+        }),
     ],
 };
